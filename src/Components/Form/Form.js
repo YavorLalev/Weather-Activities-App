1d8837e64eaf5e1c6af7e27018376af2efa9fdd0
YavorLalev <yavorlalev@abv.bg>
2024-01-10 12:02:39 +0100

    Handle Event Submit Function

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,4 +1,11 @@
 export default function Form({ onAddActivity }) {
+  function handleEventSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onAddActivity(data);
+  }
   return (
     <form>
       <h1>Add new Activity:</h1>
@@ -6,7 +13,9 @@ export default function Form({ onAddActivity }) {
       <input id="name"></input>
       <label htmlFor="good-weather-activity">Good-weather activity:</label>
       <input type="checkbox" id="good-weather-activity" />
-      <button type="submit">Submit</button>
+      <button type="submit" onSubmit={handleEventSubmit}>
+        Submit
+      </button>
     </form>
   );
 }

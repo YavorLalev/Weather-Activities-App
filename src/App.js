621135ae459948c6edcce9ba0917cf8efc99c881
YavorLalev <yavorlalev@abv.bg>
2024-01-11 11:22:55 +0100

    Update List Activity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import Form from "./Components/Form/Form.js";
-import uid from "uid";
+import { uid } from "uid";
+import List from "./Components/List/List.js";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { ...newActivity, id: uid() }]);
+    console.log(activities);
   }
 
   return (
     <div>
+      <List activities={activities} />
       <Form onAddActivity={handleAddActivity} />
     </div>
   );
